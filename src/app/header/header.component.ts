e2326246d114c88950a219811ab6b5aa53678fab
chunkityip <alex0917lfo@hotmail.com>
2025-06-18 22:02:41 -0400

    change header urls and background

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from '../auth/service.guard';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
